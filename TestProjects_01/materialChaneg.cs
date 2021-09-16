c25707c8ffed9e2bcdf0579f9ddfb8073bdd9988
Hansaka Sandaruwan <60069417+H-Sandaruwan@users.noreply.github.com>
2021-09-17 01:11:45 +0530

    Create materialChaneg.cs

---
+++ TestProjects_01/materialChaneg.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialChaneg : MonoBehaviour
+{
+  public Material[] material;
+  public int x;
+  Renderer rend ;
+
+    void Start()
+    {
+        x = 0;
+        rend = GetComponent <Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial =material[x];
+
+    }
+
+    
+    void Update()
+    {
+        rend.sharedMaterial = material[x];
+    }
+
+    public void Color00()
+    {
+        x=0;
+       
+       
+
+    }
+    public void Color01()
+    {
+        x=1;
+        
+    }
+
+
+}
